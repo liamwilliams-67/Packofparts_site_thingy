afa6dcf44fb9460bc316280707be77e79377ee68
Liam Williams <liam.williams@packofparts.org>
2026-01-29 21:51:46 -0800

    Fix buttons (Louder)

--- src/Join.tsx
+++ src/Join.tsx
@@ -31,7 +31,7 @@ function Join() {
     { name: 'Home', href: '/' },
     { name: 'Join The Club', href: '/join' },
     { name: 'For Members', href: '/#members' },
-    { name: 'Community', href: '/#community' },
+    { name: 'Community', href: '/community' },
     { name: 'Donate', href: '/#donate' },
     { name: 'Contact Us', href: '/contact' },
     { name: 'Summer Camps', href: '/#camps' },
--- src/contact.tsx
+++ src/contact.tsx
@@ -35,7 +35,7 @@ function Contact() {
     { name: 'Home', href: '/' },
     { name: 'Join The Club', href: '/join' },
     { name: 'For Members', href: '/#members' },
-    { name: 'Community', href: '/#community' },
+    { name: 'Community', href: '/community' },
     { name: 'Donate', href: '/#donate' },
     { name: 'Contact Us', href: '/contact' },
     { name: 'Summer Camps', href: '/#camps' },
