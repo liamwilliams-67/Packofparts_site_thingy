069e2a064cbb8c554f93e9ad860f8595cbdb83b7
Liam Williams <liam.williams@packofparts.org>
2026-01-29 21:40:14 -0800

    Might break

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         contact: resolve(__dirname, "contact.html"),
+        join: resolve(__dirname, "join.html"),
       },
     },
   },
