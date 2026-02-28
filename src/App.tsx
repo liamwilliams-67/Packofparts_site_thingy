993d3443239210e7d23f9842515b6f6d3e4a02c1
Siddhant Lodha <57517249+lodhasid@users.noreply.github.com>
2026-02-27 18:35:19 -0800

    attempt to fix scrolling and navbar on home page

--- src/App.tsx
+++ src/App.tsx
@@ -237,7 +237,7 @@ function App() {
       {/* Navigation */}
       <nav 
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
-          isNavVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
+          true ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
         }`}
       >
         <div className="container-custom pt-4 pb-0">
